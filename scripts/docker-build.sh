e326b60cd858bc38ca7d1890b6aae00d094c49e7
Luca Zulberti <zulberti.luca@gmail.com>
2024-12-11 22:43:28 +0100

    :technologist: Improve docker build script, use 'latest' tag on 'main'

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
 
-tag=$(git rev-parse --abbrev-ref HEAD)
-
 # Docker builder name for multi-arch builds
-BUILDER_NAME="multi-arch-builder"
-
-# Check if the builder exists
-EXISTING_BUILDER=$(docker buildx ls | grep "$BUILDER_NAME" 2> /dev/null)
+builder="multi-arch-builder"
 
 # If the builder does not exist, create a new one
-if [ -z "$EXISTING_BUILDER" ]; then
+if [ -z "$(docker buildx ls | grep "$builder" 2> /dev/null)" ]; then
     echo "Creating the builder..."
-    docker buildx create --name "$BUILDER_NAME" --use
+    docker buildx create --name "$builder" --use
 else
-    echo "Using existing builder: $BUILDER_NAME"
+    echo "Using existing builder: $builder"
 fi
 
 # Build the Docker image for multiple platforms
-PLATFORMS="linux/amd64,linux/arm64"
-echo "Building the Docker image for platforms: ${PLATFORMS}"
-docker buildx build \
-    --platform $PLATFORMS \
-    -t lucazulberti/workenv:${tag} \
-    .
+platforms="linux/amd64,linux/arm64"
+name=lucazulberti/workenv
 
+# Build arguments
+docker_build_args=(--platform $platforms)
+
+# If branch is main, tag the image as latest
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$branch" == "main" ]; then
+    docker_build_args+=(-t ${name}:latest)
+else
+    # Otherwise, tag the image with the branch name
+    docker_build_args+=(-t ${name}:${branch})
+fi
+
+echo "${docker_build_args[@]}"
+
+# Build the Docker image, passing additional arguments
+echo "Building the Docker image for platforms: ${platforms}"
+docker buildx build "${docker_build_args[@]}" "$@" .
 echo "Docker image built successfully!"
